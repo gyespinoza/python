5d551f37c77042302d834761d9d43d7ebfa2a6be
giselaYasmin <80352971+giselaYasmin@users.noreply.github.com>
2022-07-18 14:30:09 -0600

    Ejercicio 2 tema 7

---
+++ tema7/ejercicio2.py
@@ -0,0 +1,11 @@
+import time
+
+
+hora = time.strftime('%H')
+minutos = time.strftime('%M')
+
+
+if int(hora) >= 19: 
+    print("Hora de irse a casa")
+else:
+    print("Quedan {} horas y {} minutos para irse a casa".format(18-int(hora), 59-int(minutos)))
