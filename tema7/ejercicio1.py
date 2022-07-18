c1e1751c7ac875c8d3527ad5d56856e3b80a8eb6
giselaYasmin <80352971+giselaYasmin@users.noreply.github.com>
2022-07-18 14:24:49 -0600

    Ejercicio 1 tema 7

---
+++ tema7/ejercicio1.py
@@ -0,0 +1,9 @@
+from operaciones import *
+
+numero1 = 5
+numero2 = 6
+
+print(sumar(numero1, numero2))
+print(restar(numero1, numero2))
+print(multiplicar(numero1, numero2))
+print(dividir(numero1, numero2))
